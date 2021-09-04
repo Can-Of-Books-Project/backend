d8484fea2dd713ff23fc0bb9c8ea8d0a07f6177a
fadiHB <eng.fadi.hb@gmail.com>
2021-09-04 18:25:35 +0300

    mognodb url

--- server.js
+++ server.js
@@ -15,21 +15,21 @@ app.get('', (request, response) => {
     response.status(200).send("Ok")
 })
 
-// const books = require("./modules/Books.js");
-const booksRouter = require('./routes/booksRouter.js')
 
 const seedUsers = require('./seeds/seedUsers');
 // seedUsers()
 
-
+// way 1
 // CRUD application
+// const books = require("./modules/Books.js");
 // app.get('/books', Book.list);
 // app.post('/books', Book.add);
 // app.delete('/books/:id', Book.delete);
 // app.put('/books/:id', Book.update);
 
 
-// 2. web app (API)
+// way 2. web app (API)
+const booksRouter = require('./routes/booksRouter.js')
 app.use('/api', booksRouter);
 
 
@@ -37,10 +37,10 @@ app.use('/api', booksRouter);
 
 
 const PORT = process.env.PORT
-const DATABASE_URL_LOCAL = process.env.DATABASE_URL_LOCAL
+const DATABASE_URL = process.env.DATABASE_URL
 app.listen(PORT, () => {
     console.log(`listening on PORT: ${PORT}`);
-    mongoose.connect(DATABASE_URL_LOCAL, {useNewUrlParser: true, useUnifiedTopology: true});
+    mongoose.connect(DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true});
     const db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error:'));
     db.once('open', function() {
