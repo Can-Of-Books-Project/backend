6956244cd198cab837bad8665ef84f4c7379eadd
fadiHB <eng.fadi.hb@gmail.com>
2021-09-06 09:46:03 +0300

    init the middleware

--- middleware/loggerMiddleware.js
+++ middleware/loggerMiddleware.js
@@ -2,6 +2,7 @@
 // No need for req and res parameters if you wont use it
 const myLogger = function (req, res, next) {
     if (!req.query.length) {
+        // status code : 500 (Internal Server Error)
         next("Access Denied")
     }else{
         
