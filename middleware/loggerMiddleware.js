7141bfb78930e4d6af6747351e902c95d206cee0
fadiHB <eng.fadi.hb@gmail.com>
2021-09-07 00:48:27 +0300

    api authenticated

--- middleware/loggerMiddleware.js
+++ middleware/loggerMiddleware.js
@@ -1,14 +1,32 @@
 
+const [User] = require('../models/User');
+
 // No need for req and res parameters if you wont use it
-const myLogger = function (req, res, next) {
-    if (!req.query.length) {
-        // status code : 500 (Internal Server Error)
-        next("Access Denied")
-    }else{
-        
-        console.log('LOGGED')
-        next()
+const myLogger = async function (req, res, next) {
+
+    let email = req.body.authorEmail;
+    if (!email) {
+        next("Only for Authenticated users");
+        return
     }
+
+    await User.find({ email }, (error, result) => {
+        if (error) {
+            console.log(error);
+            // status code : 500 (Internal Server Error)
+            next("Internal Server Error");
+        };
+
+        if (!result.length) {
+            next("Access Denied");
+        } else {
+            console.log('LOGGED');
+            next();
+        }
+
+
+    });
+
 }
 
 module.exports = myLogger;
